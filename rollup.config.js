793ff28a2dfd4ee49461f4c9bfd610bca8629d8d
Unknown <lpreterite@126.com>
2019-04-25 17:58:54 +0800

    support es5 in umd file

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import vue from 'rollup-plugin-vue'
 import css from 'rollup-plugin-css-only'
 import { terser } from 'rollup-plugin-terser'
+import babel from 'rollup-plugin-babel'
 
 import pkg from './package.json'
 
@@ -17,7 +18,6 @@ export default [
 	{
 		input: 'src/main.js',
 		output: [
-            { name: 'vpin', file: pkg.browser, format: 'umd', exports: 'named', sourcemap },
 			{ name: 'vpin', file: pkg.main, format: 'cjs', exports: 'named', sourcemap },
 			{ name: 'vpin', file: pkg.module, format: 'es', exports: 'named', sourcemap }
 		],
@@ -28,5 +28,19 @@ export default [
 			vue({ css: false }),
 			...plugins
         ]
+	},
+	{
+		input: 'src/main.js',
+		output: [
+            { name: 'vpin', file: pkg.pkg, format: 'umd', exports: 'named', sourcemap },
+		],
+        plugins: [
+			resolve(),
+            commonjs(),
+			css(),
+			vue({ css: false }),
+			babel({ exclude: 'node_modules/**', runtimeHelpers: true }),
+			...plugins
+		]
 	}
 ]
